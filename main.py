2143fd63f7d8dd54b977f8c91e4ec7181f1e7421
Pedro <92474347+Pedroffda@users.noreply.github.com>
2025-06-01 15:48:22 -0300

    Update CORS middleware to include additional allowed origins

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ app.include_router(router, prefix="/api/v1")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://lorawisep.littoralab.com.br",
+        "http://localhost:8888",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
